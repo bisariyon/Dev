2e9fe9cf8852ab443bd06e96606ee83b6b0d5d89
bisariyon <deepbisariya@gmail.com>
2023-06-17 14:29:05 +0530

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
         useMaterial3: true,
       ),
-      home: account(),
+      home: OnboardingScreen(),
     );
   }
 }
